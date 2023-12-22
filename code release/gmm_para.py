7f4026da1e8ee62c023b7a13f4dd7b592aa3f2ec
LI Jichang <46667448+lijichang@users.noreply.github.com>
2023-12-22 17:47:49 +0800

    Delete code release directory

--- code release/gmm_para.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-
-path = "user-0-gmm_parameters.pkl"
-
-    
-para = pickle.load(open(path, "rb"))
--- code release/test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-command_federated = 'python {} --components=5 --rounds=50 --dataset=blob --K=100 --C=0.1 --plots_step=10 --seed=111 --init=kmeans'.format('.\\src\\federated.py')
-command_baseline = 'python {} --components=5 --epochs=100 --dataset=blob --plots_step=20 --seed=111 --init=kmeans'.format('.\\src\\baseline.py')
-
-for _ in range(10):
-    os.system(command_federated)
